53e9585b9bff26af214193acba6c88fe47d8776f
kamikahughes <114545621+kamikahughes@users.noreply.github.com>
2023-01-26 20:27:54 -0800

    Initialize Git repositort

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request
+import numpy as np
+import pickle
+import joblib
+app = Flask(__name__)
+filename = 'week4.pkl'
+#model = pickle.load(open(filename, 'rb'))
+model = joblib.load(filename)
+#model = joblib.load(filename)
+@app.route('/')
+def index(): 
+    return render_template('index.html')
+@app.route('/predict', methods=['POST'])
+def predict():
+    Aspect_Ratio = request.form['Aspect_Ratio']
+    Uniformity = request.form['Uniformity']
+    Elongation = request.form['Elongation']
+    Solidity = request.form['Solidity']
+
+    
+      
+    pred = model.predict(np.array([[Aspect_Ratio, Uniformity, Elongation, Solidity ]]))
+    print(pred)
+    return render_template('index.html', predict=str(pred))
+
+
+if __name__ == '__main__':
+    app.run
